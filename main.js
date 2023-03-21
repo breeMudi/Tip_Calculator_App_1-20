dc004b88299bb86ee95adbdb0fed878491057ba5
breeMudi <65234249+breeMudi@users.noreply.github.com>
2023-03-21 22:31:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,57 +1,62 @@
 let bills = document.getElementById('bill')
 let people = document.getElementById('people')
-let perText = document.getElementsByClassName('normalbtn').textContent
-let percentages = 0
+let percent = 0
 
-people.addEventListener('keyup', (e) => {
+// declare and asign buttons
+const btn5 = document.getElementById('n')
+const btn10 = document.getElementById('10')
+const btn15 = document.getElementById('15')
+const btn25 = document.getElementById('25')
+const btn50 = document.getElementById('50')
+const customInput = document.getElementById('custom')
+
+
+bills.addEventListener('keyup', (e) => {
     if (e.keyCode == 13){
-        document.getElementById('billperson').textContent='$'+(bills.value/(people.value)).toFixed(2)
+        document.getElementById('billperson').textContent='$'+(bills.value)
     }
 })
 
+people.addEventListener('keyup', (e) => {
+    if (e.keyCode == 13){
+        document.getElementById('tipperson').textContent='$'+(bills.value/(people.value)).toFixed(2)
+    }
+})
 
-
-// bills.addEventListener('keyup', (e) => {
-//     if e.keycode === 
-// })
-function btn() {
-    let perText = document.getElementsByClassName('normalbtn').textContent
-    if (perText == '5%'){
-        percentages = 0.05
-    }
-    else if (perText == '10%') {
-        percentages = 0.1
-    }
-    else if (perText == '15%') {
-        percentages = 0.15
-    }
-    else if (perText == '25%') {
-        percentages = 0.25
-    }
-    else if (perText == '50%') {
-        percentages = 0.5
-    }
-    else{
-        percentages = 2
-    }
-    const billp = bills.value * percentages
-    console.log(billp)
-    document.getElementById('tipperson').textContent=billp
+// function btn(butn){
+//     percent = Number(butn.id)
+// }
+if (btn5.click){
+    percent = 0.05
+}
+else if (btn10){
+    percent = 0.1
+}
+else if (btn15){
+    percent = 0.15
+}
+else if (btn20){
+    percent = 0.2
+}
+else if (btn25){
+    percent = 0.25
+}
+else if (btn50){
+    percent = 0.5
+}
+else{
+    percent = 2
 }
 
-// if(){
-
-// }
-
-// bills.addEventListener('keyup', (e) => {
-//     if (e.keyCode == 13){
-//         document.getElementById('billperson').textContent=bills.value
-//     }
-// })
+function btn(){
+    document.getElementById('tipperson').textContent='$'+((bills.value * percent)/(people.value)).toFixed(2)
+}
+// The calculations:
+// const billp = bills.value * percentages
+//     console.log(billp)
+//     document.getElementById('tipperson').textContent=billp
 
 
 
-// if (bill){
-//     alert('your bill is ' + bill)
-// } 
-// const percentages = [{order:10}, {order:15}, {order:20}]
+// document.getElementById('billperson').textContent='$'+(bills.value/(people.value)).toFixed(2)
+
